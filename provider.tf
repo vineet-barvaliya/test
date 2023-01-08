c03c11f8c1c614b9190831817c021a25286b85c4
vineetbarvaliya <vineet.barvaliya@avanade.com>
2023-01-09 10:26:10 +1100

    Remove SP credentials

--- provider.tf
+++ provider.tf
@@ -10,9 +10,4 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-
-  subscription_id = "725d846f-031b-4069-9f31-50414f130222"
-  client_id       = "7fd23cf3-49be-45ed-973c-cdd5ba015ebe"
-  client_secret   = "X1e8Q~2FfBGFanvKxQZnoGCQvbgwuE9DbVssxcGG"
-  tenant_id       = "5488ef43-b143-480d-ba01-51e81582efb1"
 }
